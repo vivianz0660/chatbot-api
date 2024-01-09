1ff00a0a8bb110424eb0f7a2699c5ae1784bbe9b
vivianz0660 <146015178+vivianz0660@users.noreply.github.com>
2024-01-09 14:19:08 -0600

    Update Topics.java

--- chatbot-api-main/chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/zsxq/model/vo/Topics.java
+++ chatbot-api-main/chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/zsxq/model/vo/Topics.java
@@ -1,11 +1,5 @@
 package cn.bugstack.chatbot.api.domain.zsxq.model.vo;
 
-/**
- * @author 小傅哥，微信：fustack
- * @description
- * @github https://github.com/fuzhengwei
- * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class Topics {
 
     private String topic_id;
