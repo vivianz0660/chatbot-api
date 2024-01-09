192ed4bfbda72918ddce8b8c0d1b75b2209636c9
vivianz0660 <146015178+vivianz0660@users.noreply.github.com>
2024-01-09 14:16:04 -0600

    Update Choices.java

--- chatbot-api-main/chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/ai/model/vo/Choices.java
+++ chatbot-api-main/chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/ai/model/vo/Choices.java
@@ -1,11 +1,6 @@
 package cn.bugstack.chatbot.api.domain.ai.model.vo;
 
-/**
- * @author 小傅哥，微信：fustack
- * @description 选择
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
+
 public class Choices {
 
     private String text;
