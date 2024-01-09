514b58702354d9e85ac0dddb514a7c13ef2d7d3c
vivianz0660 <146015178+vivianz0660@users.noreply.github.com>
2024-01-09 14:15:42 -0600

    Update AIAnswer.java

--- chatbot-api-main/chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/ai/model/aggregates/AIAnswer.java
+++ chatbot-api-main/chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/ai/model/aggregates/AIAnswer.java
@@ -4,12 +4,7 @@ import cn.bugstack.chatbot.api.domain.ai.model.vo.Choices;
 
 import java.util.List;
 
-/**
- * @author 小傅哥，微信：fustack
- * @description AI Answer
- * @github https://github.com/fuzhengwei
- * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
+
 public class AIAnswer {
 
     private String id;
